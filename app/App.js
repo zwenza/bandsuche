7550496ea92c4397d16c5a647bf3e94b06f54f67
David Jöch <david.joech@catalysts.cc>
2017-04-02 17:37:40 +0200

    added styled-components

--- app/App.js
+++ app/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+import styled from 'styled-components/native';
+
+const Header = styled.Text`
+  font-size: 25
+`;
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hi :)</Text>
+      <View>
+        <Header>Hi :)</Header>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
